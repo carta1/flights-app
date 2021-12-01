d7b38338c653a61e9a11f21e5ecc995a0a5ea557
Jair Martinez <jair.martinez@incentro.com>
2021-12-01 16:18:45 +0100

    adjsutments to navigation

--- flights_app/app/src/main/java/com/jmart/flights_app/MainActivity.kt
+++ flights_app/app/src/main/java/com/jmart/flights_app/MainActivity.kt
@@ -6,22 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.BlendMode.Companion.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -48,7 +42,7 @@ class MainActivity : ComponentActivity() {
             Flights_appTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    BottomNavigation()
+                    mainPageContent()
                 }
             }
         }
@@ -59,8 +53,8 @@ class MainActivity : ComponentActivity() {
 @ExperimentalComposeUiApi
 @ExperimentalFoundationApi
 @Composable
-fun BottomNavigation() {
-    val items = listOf(NavScreens.Map, NavScreens.Airport, NavScreens.Settings)
+fun mainPageContent() {
+    val bottomTabBarItems = listOf(NavScreens.Map, NavScreens.Airport, NavScreens.Settings)
 
     val navController = rememberNavController()
     val navBackStackEntry by navController.currentBackStackEntryAsState()
@@ -77,67 +71,11 @@ fun BottomNavigation() {
             )
         },
         bottomBar = {
+            // it does not show the nav bar when the user is in the airport detail page
             if (currentRoute != NavScreens.AirportDetails.route) {
-                BottomNavigation {
-                    val currentDestination = navBackStackEntry?.destination
-                    items.forEach { screen ->
-                        BottomNavigationItem(
-                            icon = { Icon(screen.icon, contentDescription = null) },
-                            label = { Text(stringResource(screen.resourceId)) },
-                            selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
-                            onClick = {
-                                navController.navigate(screen.route) {
-                                    // Pop up to the start destination of the graph to
-                                    // avoid building up a large stack of destinations
-                                    // on the back stack as users select items
-                                    popUpTo(navController.graph.findStartDestination().id) {
-                                        saveState = true
-                                    }
-                                    // Avoid multiple copies of the same destination when
-                                    // reselecting the same item
-                                    launchSingleTop = true
-                                    // Restore state when reselecting a previously selected item
-                                    restoreState = true
-                                }
-                            }
-                        )
-                    }
-
-                }
-
+                bottomNavigationComponent(navController, bottomTabBarItems, navBackStackEntry)
             }
         }
-
-//        {
-//            BottomNavigation {
-//                val navBackStackEntry by navController.currentBackStackEntryAsState()
-//                val currentDestination = navBackStackEntry?.destination
-//                items.forEach { screen ->
-//                    BottomNavigationItem(
-//                        icon = { Icon(screen.icon, contentDescription = null) },
-//                        label = { Text(stringResource(screen.resourceId)) },
-//                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
-//                        onClick = {
-//                            navController.navigate(screen.route) {
-//                                // Pop up to the start destination of the graph to
-//                                // avoid building up a large stack of destinations
-//                                // on the back stack as users select items
-//                                popUpTo(navController.graph.findStartDestination().id) {
-//                                    saveState = true
-//                                }
-//                                // Avoid multiple copies of the same destination when
-//                                // reselecting the same item
-//                                launchSingleTop = true
-//                                // Restore state when reselecting a previously selected item
-//                                restoreState = true
-//                            }
-//                        }
-//                    )
-//                }
-//
-//            }
-//
-//        }
     ) { innerPadding ->
         // set up navHost and bottom navigation
         NavHost(
@@ -163,5 +101,35 @@ fun BottomNavigation() {
                 }
             }
         }
+    }
+}
+
+@Composable
+fun bottomNavigationComponent(navController: NavHostController, bottomTabBarItems: List<NavScreens>, navBackStackEntry: NavBackStackEntry? ) {
+    BottomNavigation {
+        val currentDestination = navBackStackEntry?.destination
+        bottomTabBarItems.forEach { screen ->
+            BottomNavigationItem(
+                icon = { Icon(screen.icon, contentDescription = null) },
+                label = { Text(stringResource(screen.resourceId)) },
+                selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                onClick = {
+                    navController.navigate(screen.route) {
+                        // Pop up to the start destination of the graph to
+                        // avoid building up a large stack of destinations
+                        // on the back stack as users select items
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        // Avoid multiple copies of the same destination when
+                        // reselecting the same item
+                        launchSingleTop = true
+                        // Restore state when reselecting a previously selected item
+                        restoreState = true
+                    }
+                }
+            )
+        }
+
     }
 }
